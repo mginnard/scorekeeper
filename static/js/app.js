35a5ec1b000996b57a387809c9938059b39c5c40
Matthew Ginnard <matthew.ginnard@gmail.com>
2016-03-30 22:05:35 -0700

    set up assets directory, scss + js, link js file in index.html

---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() { // Makes sure the document has completed loading before executing anything wrapped inside.
+
+    // document.getElementById("toggle-animation").addEventListener("click", function() {
+    //         document
+    //             .getElementById("animated-div")
+    //             .classList
+    //             .toggle("open");
+    //     });
+
+    // var animatedDivs = document
+    //     .getElementsByClassName("animated-divs");
+
+    // for (var i = 0; i < animatedDivs.length; i++) {
+    //     animatedDivs[i].addEventListener("click", function(){
+    //         this
+    //             .classList
+    //             .toggle("rotate-animate");
+    //         this
+    //             .innerHTML = "<p>Animation!</p>";
+    //     });
+    // }
+
+    
+});
